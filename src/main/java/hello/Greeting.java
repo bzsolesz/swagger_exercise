dec44886005a215560f09095905878b378f71473
baloghzs <zsolt.balogh@ubs.com>
2016-12-14 23:18:46 +0000

    Greeting POJO was added.

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,26 @@
+package hello;
+
+/**
+ * Created by zsoltbalogh on 14/12/2016.
+ */
+public class Greeting
+{
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content)
+    {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+}
