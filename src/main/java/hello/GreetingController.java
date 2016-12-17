ba966bce553e50d778aa21d53c9f7d9f66021525
baloghzs <zsolt.balogh@ubs.com>
2016-12-17 16:55:49 +0000

    Project is ready for maven build (command line, intellij) + spring boot dependencies

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zsoltbalogh on 17/12/2016.
+ */
+@RestController
+public class GreetingController
+{
+}
