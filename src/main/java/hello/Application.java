9a77fca4351903ad8fb144a7be362265d615bfa9
baloghzs <zsolt.balogh@ubs.com>
2016-12-18 00:00:48 +0000

    working greeting rest webservice was added

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zsoltbalogh on 17/12/2016.
+ */
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] arguments) {
+        SpringApplication.run(Application.class, arguments);
+    }
+}
