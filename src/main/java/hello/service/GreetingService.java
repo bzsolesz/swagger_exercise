5f2f8b6fd421803418cc230584e1ead128ecfa42
bzsolesz <bzsolesz@users.noreply.github.com>
2016-12-24 17:52:50 +0000

    GreetingServiceTest was added

--- src/main/java/hello/service/GreetingService.java
+++ src/main/java/hello/service/GreetingService.java
@@ -1,7 +1,6 @@
 package hello.service;
 
 import hello.model.Greeting;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
---
+++ src/test/java/hello/service/GreetingServiceTest.java
@@ -0,0 +1,55 @@
+package hello.service;
+
+import hello.model.Greeting;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zsolt_balogh on 24/12/2016.
+ */
+public class GreetingServiceTest {
+
+    private GreetingService testedService;
+
+    @Before
+    public void setup() {
+        testedService = new GreetingService();
+    }
+
+    @Test
+    public void shouldReturnNonNullGreeting() {
+        Greeting resultGreeting = testedService.sayGreeting(Optional.<String>empty());
+
+        assertNotNull(resultGreeting);
+    }
+
+    @Test
+    public void shouldReturnGreetingWithDefaultName() {
+        Greeting resultGreeting = testedService.sayGreeting(Optional.<String>empty());
+
+        assertEquals("Hello World!", resultGreeting.getContent());
+    }
+
+    @Test
+    public void shouldIncreaseRequestIdWithEachCall() {
+        Greeting resultGreeting = testedService.sayGreeting(Optional.<String>empty());
+
+        assertEquals(1, resultGreeting.getId());
+
+        resultGreeting = testedService.sayGreeting(Optional.<String>empty());
+
+        assertEquals(2, resultGreeting.getId());
+    }
+
+    @Test
+    public void shouldReturnGreetingWithRequestedName() {
+        Greeting resultGreeting = testedService.sayGreeting(Optional.of("TestName"));
+
+        assertEquals("Hello TestName!", resultGreeting.getContent());
+    }
+}
